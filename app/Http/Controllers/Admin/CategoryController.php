df0367b61d8839dd83dfd00a27ba43fac8c0e6c7
najadams <najmadams1706@gmail.com>
2023-03-26 13:16:58 +0000

    created views for the different components and made corrections in route

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.menus.index');
+        return view('admin.categories.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,9 +36,9 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
     Route::get('/' ,[AdminController::class, 'index'])->name("index");
     Route::resource('/categories', CategoryController::class);
-    Route::resource('/reservation' ,ReservationController::class);
-    Route::resource('/table' ,TableController::class);
-    Route::resource('/menu', MenuController::class);
+    Route::resource('/reservations' , ReservationController::class);
+    Route::resource('/tables' ,TableController::class);
+    Route::resource('/menus', MenuController::class);
 });
 
 require __DIR__.'/auth.php';
