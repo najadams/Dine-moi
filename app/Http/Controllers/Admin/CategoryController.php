4f34b2892fb808f23ca100957239679ab042ca05
najadams <najmadams1706@gmail.com>
2023-03-26 12:03:48 +0000

    made some spell checks

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.menu.index');
+        return view('admin.menus.index');
     }
 
     /**
--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -12,7 +12,7 @@ class MenuController extends Controller
      */
     public function index()
     {
-        return view('admin.menu.index');
+        return view('admin.menus.index');
     }
 
     /**
