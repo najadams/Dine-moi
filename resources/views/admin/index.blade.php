e92a6feaa3417bdf81c2fdfa8e5fe7471f62ad96
najadams <najmadams1706@gmail.com>
2023-03-26 07:57:05 +0000

    created admin page, controller, and middleware

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,25 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+<body class="antialiased bg-gray-100 dark-mode:bg-gray-900">
+    <div class="flex-col w-full md:flex md:flex-row md:min-h-screen">
+        <div @click.away="open = false" class="flex flex-col flex-shrink-0 w-full text-gray-700 bg-white md:w-64 dark-mode:text-gray-200 dark-mode:bg-gray-800" x-data="{ open: false }">
+        </div>
+    </div>
+</body>
+</x-admin-layout>
