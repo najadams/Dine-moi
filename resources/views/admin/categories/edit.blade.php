143af3d612bb41acee56c92d9be6d4936e5e0002
najadams <najmadams1706@gmail.com>
2023-05-21 20:24:51 +0000

    added page for category edit

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -28,20 +28,20 @@
                 
                 
                     <div class="mb-6 p-2">
-                      <label class="block mb-2 py-3 text-sm font-medium text-gray-900 dark:text-white" for="image">Upload image</label>
                       <div>
-                        <img class="w-32 h-32" src="{{Storage::url($category->id)}}" alt="">
+                        <img class="w-32 h-32" src="{{Storage::url($category->image)}}" alt="">
                       </div>
+                      <label class="block mb-2 py-3 text-sm font-medium text-gray-900 dark:text-white" for="image">Upload image</label>
                       <input type="file" required name= "image" name="image" class="block w-full text-sm p-2 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="      ">
                     </div>
                 
                     <div class="mb-6">
                       <label for="description" class="block py-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                      <input type="text" required id="description" name="description" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <input type="text" value="{{$category->description}}" required id="description" name="description" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </div>
 
                     <div class="mt-6 p-4">
-                      <button type="submit" value="{{$category->description}}" required class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Update</button>
+                      <button type="submit"  required class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Update</button>
                     </div>
                   </form>
   
